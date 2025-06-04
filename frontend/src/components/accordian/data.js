a85665a12741d897c9509c32eeb24b879810215a
Joel Scaria <joelscaria241444@gmail.com>
2025-06-04 14:29:27 +0530

    New file

---
+++ frontend/src/components/accordian/data.js
@@ -0,0 +1,30 @@
+
+  const data= [
+  {
+    id: 1,
+    question: "What is React?",
+    answer: "React is a JavaScript library for building user interfaces, maintained by Facebook."
+  },
+  {
+    id: 2,
+    question: "What is JSX?",
+    answer: "JSX stands for JavaScript XML. It allows you to write HTML in React."
+  },
+  {
+    id: 3,
+    question: "What is a component?",
+    answer: "A component is a reusable piece of UI in React. It can be a function or a class."
+  },
+  {
+    id: 4,
+    question: "What is state in React?",
+    answer: "State is an object that holds dynamic data and determines the behavior of a component."
+  },
+  {
+    id: 5,
+    question: "What are props in React?",
+    answer: "Props are inputs to components. They are passed as arguments and used to render dynamic content."
+  }
+];
+
+export default data;
